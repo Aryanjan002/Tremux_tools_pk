8b4f578c7bfb9b9eb5ee98a05837630c8c2d9a33
Aryanjan002 <74414155+Aryanjan002@users.noreply.github.com>
2020-11-15 08:07:32 +0430

    Create Aryan.sh

---
+++ Aryan.sh
@@ -0,0 +1,77 @@
+echo ""
+echo ""
+echo ""
+echo "The script is  made by Aryan Hacker"
+echo ""
+echo ""
+echo ""
+echo "subscribe  our channel"
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+pkg update -y                    
+pkg install python -y           
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+pkg install wcalc -y 
+pkg install openssl -y
+pkg install bmon -y
+pkg upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "╭━━━┳━━━┳╮╱╱╭┳━━━┳━╮╱╭╮
+┃╭━╮┃╭━╮┃╰╮╭╯┃╭━╮┃┃╰╮┃┃
+┃┃╱┃┃╰━╯┣╮╰╯╭┫┃╱┃┃╭╮╰╯┃
+┃╰━╯┃╭╮╭╯╰╮╭╯┃╰━╯┃┃╰╮┃┃
+┃╭━╮┃┃┃╰╮╱┃┃╱┃╭━╮┃┃╱┃┃┃
+╰╯╱╰┻╯╰━╯╱╰╯╱╰╯╱╰┻╯╱╰━╯
+
+
+
+██╗░░██╗░█████╗░░█████╗░██╗░░██╗███████╗██████╗░
+██║░░██║██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+███████║███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝
+██╔══██║██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
+██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║
+╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝"
+echo " telegram chanel  : @Aryan Hack
+youtube chanel Aryan Hack  
+github ; https://github.com/Aryanjan002"
+echo ""
+echo " this sceipt craeted by Aryan Hacker👌😌❤🙋‍♂️   Subscribe our Youtube channel and press Bell icon"
+exit
